3ffe74d853a0f253971ade59411975785947fe3b
liuhaihua <jxausea@163.com>
2024-05-14 19:50:14 +0800

    springboot-with-dynamic-datasource

--- dynamic-datasource/src/main/java/com/et/dynamic/datasource/service/userinfo/imp/UserInfoServiceImp.java
+++ dynamic-datasource/src/main/java/com/et/dynamic/datasource/service/userinfo/imp/UserInfoServiceImp.java
@@ -9,27 +9,23 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
- * <p>
- *  服务实现类
- * </p>
- *
  * @author stopping
  * @since 2024-05-13
  */
 @Service
 @DS("slave_1")
 public class UserInfoServiceImp extends ServiceImpl<UserInfoMapper, UserInfo> implements UserInfoService {
-    @Autowired
+    @Resource
     UserInfoMapper userInfoMapper;
     @Override
     @DS("master")
     public List<UserInfo> testQueryWrapper(int age) {
         QueryWrapper<UserInfo> userQueryWrapper = new QueryWrapper<>();
         userQueryWrapper.ge("age", age);
-        //trainList为空不报错
         List<UserInfo> userList = userInfoMapper.selectList(userQueryWrapper);
         return userList;
     }
