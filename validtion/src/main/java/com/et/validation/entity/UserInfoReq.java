1f96576192f873abcb2b7d2e68a8fc69bd478dad
liuhaihua <jxausea@163.com>
2024-05-21 22:27:05 +0800

    springboot-with-readme

--- validtion/src/main/java/com/et/validation/entity/UserInfoReq.java
+++ validtion/src/main/java/com/et/validation/entity/UserInfoReq.java
@@ -10,18 +10,18 @@ import javax.validation.constraints.*;
 @ToString
 public class UserInfoReq {
 
-    @NotNull(message = "id不能为null")
+    @NotNull(message = "id  is null")
     private Long id;
 
-    @NotBlank(message = "username不能为空")
+    @NotBlank(message = "username is null")
     private String username;
 
-    @NotNull(message = "age不能为null")
-    @Min(value = 1, message = "年龄不符合要求")
-    @Max(value = 200, message = "年龄不符合要求")
+    @NotNull(message = "age is null")
+    @Min(value = 1, message = "min age is 1 ")
+    @Max(value = 200, message = "max age is 200")
     private Integer age;
-    @Email(message = "邮箱不符合规范")
+    @Email(message = "email format limit")
     private String email;
-    @IpAddress(message = "ip不符合规范")
+    @IpAddress(message = "ip format  limit")
     private String ip;
 }
