c721b271a0acd08cffe5132777c8e515ac7eae89
liuhaihua <liuahaihua@idadt.com>
2024-06-12 15:20:08 +0800

    springboot-with-vavr

---
+++ WebRTC/src/main/java/com/et/webrtc/controller/HelloWorldController.java
@@ -0,0 +1,29 @@
+package com.et.webrtc.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping("/hello")
+    public Map<String, Object> showHelloWorld(){
+        Map<String, Object> map = new HashMap<>();
+        map.put("msg", "HelloWorld");
+        return map;
+    }
+    /**
+     * WebRTC + WebSocket
+     */
+    @RequestMapping("webrtc/{username}.html")
+    public ModelAndView socketChartPage(@PathVariable String username) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("webrtc.html");
+        modelAndView.addObject("username",username);
+        return modelAndView;
+    }
+}
