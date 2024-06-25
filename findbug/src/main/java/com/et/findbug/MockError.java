d5f2d0924e5d56fcefaa60115925cd67eb8c650a
liuhaihua <liuahaihua@idadt.com>
2024-06-25 17:31:50 +0800

    springboot-with-findbug

---
+++ findbug/src/main/java/com/et/findbug/MockError.java
@@ -0,0 +1,16 @@
+package com.et.findbug;
+
+/**
+ * @author liuhaihua
+ * @version 1.0
+ * @ClassName MockError
+ * @Description todo
+ * @date 2024/06/25/ 17:30
+ */
+
+public class MockError {
+    private static String dbrBO;
+    public final void refresh() {
+        dbrBO = null;
+    }
+}
