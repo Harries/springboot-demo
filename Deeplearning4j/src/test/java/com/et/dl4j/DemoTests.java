1eefd524cf8b71316da3a5314d39ad43843059f3
liuhaihua <liuahaihua@idadt.com>
2024-06-28 17:51:58 +0800

    springboot-with-deeplearning4j

---
+++ Deeplearning4j/src/test/java/com/et/dl4j/DemoTests.java
@@ -0,0 +1,33 @@
+package com.et.dl4j;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DemoApplication.class)
+public class DemoTests {
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+
+    @Before
+    public void before()  {
+        log.info("init some data");
+    }
+    @After
+    public void after(){
+        log.info("clean some data");
+    }
+    @Test
+    public void execute()  {
+        log.info("your method test Code");
+    }
+
+}
+
