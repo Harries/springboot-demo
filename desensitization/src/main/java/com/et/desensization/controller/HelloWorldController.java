88078424d1843f5b0edcd3ebc0a3e078f480e7b6
liuhaihua <jxausea@163.com>
2024-08-11 22:10:13 +0800

    springboot-with-datadesensization

--- desensitization/src/main/java/com/et/desensization/controller/HelloWorldController.java
+++ desensitization/src/main/java/com/et/desensization/controller/HelloWorldController.java
@@ -13,7 +13,7 @@ public class HelloWorldController {
     @RequestMapping("/hello")
     public Teacher showHelloWorld(){
 
-		Teacher teacher = Sensitive.desensitize(new Teacher(1,"小明老师","400000000000000000","15900000000","1420000000@qq.com","123456789"));
+		Teacher teacher = Sensitive.desensitize(new Teacher(1,"Harries BLOG","400000000000000000","15900000000","1420000000@qq.com","123456789"));
 		return teacher;
     }
 }
