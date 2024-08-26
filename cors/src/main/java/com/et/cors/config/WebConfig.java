736f04758385f74e7f7d6baaa4728c2ca9d78a32
liuhaihua <jxausea@163.com>
2024-08-26 22:11:04 +0800

    springboot-with-cors

--- cors/src/main/java/com/et/cors/config/WebConfig.java
+++ cors/src/main/java/com/et/cors/config/WebConfig.java
@@ -1,17 +1,17 @@
-package com.et.cors.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // 所有接口
-                .allowCredentials(true) // 是否发送 Cookie
-                .allowedOrigins("*") // 支持域
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // 支持方法
-                .allowedHeaders("*");
-    }
-}
+//package com.et.cors.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowCredentials(true)
+//                .allowedOrigins("*")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                .allowedHeaders("*");
+//    }
+//}
