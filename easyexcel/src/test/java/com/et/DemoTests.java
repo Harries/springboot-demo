77b23c29c1cd6b7f63810244d1a86e23e7f28d10
liuhaihua <liuahaihua@idadt.com>
2024-11-14 14:07:52 +0800

    springboot-with-easyexcel

---
+++ easyexcel/src/test/java/com/et/DemoTests.java
@@ -0,0 +1,155 @@
+package com.et;
+
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.enums.CellDataTypeEnum;
+import com.alibaba.excel.metadata.data.*;
+import com.alibaba.excel.util.ListUtils;
+import com.alibaba.excel.write.handler.CellWriteHandler;
+import com.alibaba.excel.write.metadata.style.WriteCellStyle;
+import com.alibaba.excel.write.metadata.style.WriteFont;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DemoApplication.class)
+public class DemoTests {
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    List<DemoData> list = ListUtils.newArrayList();
+    @Before
+    public void before()  {
+        log.info("init some data");
+        for (int i = 0; i < 10; i++) {
+            DemoData data = new DemoData();
+            data.setString("字符串" + i);
+            data.setDate(new Date());
+            data.setDoubleData(0.56);
+            list.add(data);
+        }
+    }
+    @After
+    public void after(){
+        log.info("clean some data");
+    }
+    @Test
+    public void execute()  {
+        log.info("your method test Code");
+    }
+    /**
+     * 超链接、备注、公式、指定单个单元格的样式、单个单元格多种样式
+     * <p>
+     * 1. 创建excel对应的实体对象 参照{@link WriteCellDemoData}
+     * <p>
+     * 2. 直接写即可
+     *
+     * @since 3.0.0-beta1
+     */
+    @Test
+    public void writeCellDataWrite() {
+        String fileName = TestFileUtil.getPath() + "writeCellDataWrite" + System.currentTimeMillis() + ".xlsx";
+        System.out.println(fileName);
+        WriteCellDemoData writeCellDemoData = new WriteCellDemoData();
+
+        // 设置超链接
+        WriteCellData<String> hyperlink = new WriteCellData<>("官方网站");
+        writeCellDemoData.setHyperlink(hyperlink);
+        HyperlinkData hyperlinkData = new HyperlinkData();
+        hyperlink.setHyperlinkData(hyperlinkData);
+        hyperlinkData.setAddress("https://github.com/alibaba/easyexcel");
+        hyperlinkData.setHyperlinkType(HyperlinkData.HyperlinkType.URL);
+
+        // 设置备注
+        WriteCellData<String> comment = new WriteCellData<>("备注的单元格信息");
+        writeCellDemoData.setCommentData(comment);
+        CommentData commentData = new CommentData();
+        comment.setCommentData(commentData);
+        commentData.setAuthor("Jiaju Zhuang");
+        commentData.setRichTextStringData(new RichTextStringData("这是一个备注"));
+        // 备注的默认大小是按照单元格的大小 这里想调整到4个单元格那么大 所以向后 向下 各额外占用了一个单元格
+        commentData.setRelativeLastColumnIndex(1);
+        commentData.setRelativeLastRowIndex(1);
+
+        // 设置公式
+        WriteCellData<String> formula = new WriteCellData<>();
+        writeCellDemoData.setFormulaData(formula);
+        FormulaData formulaData = new FormulaData();
+        formula.setFormulaData(formulaData);
+        // 将 123456789 中的第一个数字替换成 2
+        // 这里只是例子 如果真的涉及到公式 能内存算好尽量内存算好 公式能不用尽量不用
+        formulaData.setFormulaValue("REPLACE(123456789,1,1,2)");
+
+        // 设置单个单元格的样式 当然样式 很多的话 也可以用注解等方式。
+        WriteCellData<String> writeCellStyle = new WriteCellData<>("单元格样式");
+        writeCellStyle.setType(CellDataTypeEnum.STRING);
+        writeCellDemoData.setWriteCellStyle(writeCellStyle);
+        WriteCellStyle writeCellStyleData = new WriteCellStyle();
+        writeCellStyle.setWriteCellStyle(writeCellStyleData);
+        // 这里需要指定 FillPatternType 为FillPatternType.SOLID_FOREGROUND 不然无法显示背景颜色.
+        writeCellStyleData.setFillPatternType(FillPatternType.SOLID_FOREGROUND);
+        // 背景绿色
+        writeCellStyleData.setFillForegroundColor(IndexedColors.GREEN.getIndex());
+
+        // 设置单个单元格多种样式
+        // 这里需要设置 inMomery=true 不然会导致无法展示单个单元格多种样式，所以慎用
+        WriteCellData<String> richTest = new WriteCellData<>();
+        richTest.setType(CellDataTypeEnum.RICH_TEXT_STRING);
+        writeCellDemoData.setRichText(richTest);
+        RichTextStringData richTextStringData = new RichTextStringData();
+        richTest.setRichTextStringDataValue(richTextStringData);
+        richTextStringData.setTextString("红色绿色默认");
+        // 前2个字红色
+        WriteFont writeFont = new WriteFont();
+        writeFont.setColor(IndexedColors.RED.getIndex());
+        richTextStringData.applyFont(0, 2, writeFont);
+        // 接下来2个字绿色
+        writeFont = new WriteFont();
+        writeFont.setColor(IndexedColors.GREEN.getIndex());
+        richTextStringData.applyFont(2, 4, writeFont);
+
+        List<WriteCellDemoData> data = new ArrayList<>();
+        data.add(writeCellDemoData);
+        EasyExcel.write(fileName, WriteCellDemoData.class).inMemory(true).sheet("模板").doWrite(data);
+    }
+    /**
+     * 自定义拦截器.对第一列第一行和第二行的数据新增下拉框，显示 测试1 测试2
+     */
+    @Test
+    public void customHandlerWrite() {
+        String fileName = TestFileUtil.getPath() + "customHandlerWrite" + System.currentTimeMillis() + ".xlsx";
+        // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
+        EasyExcel.write(fileName, DemoData.class).registerWriteHandler(new CustomSheetWriteHandler(list.size()))
+                .registerWriteHandler(new CustomCellWriteHandler()).sheet("模板").doWrite(list);
+    }
+    /**
+     * 插入批注
+     * <p>
+     * 1. 创建excel对应的实体对象 参照{@link DemoData}
+     * <p>
+     * 2. 注册拦截器 {@link CommentWriteHandler}
+     * <p>
+     * 2. 直接写即可
+     */
+    @Test
+    public void commentWrite() {
+        String fileName = TestFileUtil.getPath() + "commentWrite" + System.currentTimeMillis() + ".xlsx";
+        // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
+        // 这里要注意inMemory 要设置为true，才能支持批注。目前没有好的办法解决 不在内存处理批注。这个需要自己选择。
+        EasyExcel.write(fileName, DemoData.class).inMemory(Boolean.TRUE).registerWriteHandler(new CommentWriteHandler())
+                .sheet("模板").doWrite(list);
+    }
+}
+
---
+++ easyexcel/src/test/java/com/et/WriteCellDemoData.java
@@ -0,0 +1,46 @@
+package com.et;
+
+import com.alibaba.excel.metadata.data.WriteCellData;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@EqualsAndHashCode
+public class WriteCellDemoData {
+    /**
+     * 超链接
+     *
+     * @since 3.0.0-beta1
+     */
+    private WriteCellData<String> hyperlink;
+
+    /**
+     * 备注
+     *
+     * @since 3.0.0-beta1
+     */
+    private WriteCellData<String> commentData;
+
+    /**
+     * 公式
+     *
+     * @since 3.0.0-beta1
+     */
+    private WriteCellData<String> formulaData;
+
+    /**
+     * 指定单元格的样式。当然样式 也可以用注解等方式。
+     *
+     * @since 3.0.0-beta1
+     */
+    private WriteCellData<String> writeCellStyle;
+
+    /**
+     * 指定一个单元格有多个样式
+     *
+     * @since 3.0.0-beta1
+     */
+    private WriteCellData<String> richText;
+}
