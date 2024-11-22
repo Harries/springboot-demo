78adcd6051a671a3760dfecaf17ca0f56f0c86d0
liuhaihua <liuahaihua@idadt.com>
2024-11-22 10:50:48 +0800

    springboot-with-debezium

---
+++ mongodb/docker/mongodb/scripts/mongo_setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+sleep 10
+
+mongosh --host mongo1:27017 <<EOF
+  var cfg = {
+    "_id": "myReplicaSet",
+    "version": 1,
+    "members": [
+      {
+        "_id": 0,
+        "host": "mongo1:27017",
+        "priority": 2
+      },
+      {
+        "_id": 1,
+        "host": "mongo2:27017",
+        "priority": 0
+      },
+      {
+        "_id": 2,
+        "host": "mongo3:27017",
+        "priority": 0
+      }
+    ]
+  };
+  rs.initiate(cfg);
+EOF
